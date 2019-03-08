372d5ca26d70b512aaa7f67d67c70f8431c0f8a6
Shota Harikae <shota@sorahane.me>
2019-03-08 15:52:23 +0900

    fix

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from api.felica import FeliCa
 from api.message import Message
 from api.led import Led
 
-#
-# def initialize():
-#     from pilibs.sc1602 import SC1602
-#     import socket
-#     lcd = SC1602()
-#     lcd.string("Reader is Ready", 1)
-#     lcd.string(socket.gethostbyname(socket.gethostname()), 2)
-#     lcd.cleanup()
+
+def initialize():
+    from pilibs.sc1602 import SC1602
+    import socket
+    lcd = SC1602()
+    lcd.string("Reader is Ready", 1)
+    lcd.string(socket.gethostbyname(socket.gethostname()), 2)
+    lcd.cleanup()
 
 
 app = Flask(__name__)
@@ -25,6 +25,8 @@ app.config.from_object(os.environ.get("READER_ENV"))
 api.add_resource(FeliCa, '/api/v1/card')
 api.add_resource(Message, '/api/v1/message')
 api.add_resource(Led, '/api/v1/led')
+
+initialize()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
